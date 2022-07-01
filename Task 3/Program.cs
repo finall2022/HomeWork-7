9f2344d95f0569e44031ca917ac9b5d45f867d25
Aleks Fin <finall.git2022@gmail.com>
2022-07-01 16:40:09 +0300

    начало решения 3 задачи

--- Task 3/Program.cs
+++ Task 3/Program.cs
@@ -1,2 +1,17 @@
 ﻿/* Задача 3: Задайте двумерный массив из целых чисел. 
 Найдите среднее арифметическое элементов в каждом столбце. */
+Random rndRows = new Random();
+Random rndColumns = new Random();
+int rows = rndRows.Next(2, 3);
+int columns = rndColumns.Next(2, 10);
+int[,] array = new int[rows, columns];
+Random rand = new Random();
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < columns; j++)
+    {
+        array[i, j] = rand.Next(10, 1000);
+        Console.Write($"{array[i, j]} \t");
+    }
+    Console.WriteLine();
+}
